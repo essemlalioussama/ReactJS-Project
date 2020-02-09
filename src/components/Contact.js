afaafb9bb198786e95f7b1042a7800cae50bf133
essemlalioussama <o.essemlali@gmail.com>
2020-02-09 02:41:01 +0100

    create first component

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h4>John Ford</h4>
+                <ul>
+                    <li>
+                        Email : o.essemlali@gmail.com
+                    </li>
+                    <li>
+                        Phone : 0617097894
+                    </li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+export default Contact ;
