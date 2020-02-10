1176620953a87414886fd1a288baeefcd434dfdf
essemlalioussama <o.essemlali@gmail.com>
2020-02-10 01:22:00 +0100

    using consumer and provider

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import './App.css';
 import Contacts from './components/Contacts';
 import Header from './components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from './context'
 function App() {
 
   return (
-    <div className="App">
+    <Provider>
+      <div className="App">
 
-      <Header branding="Contact Manager" />
-      <div className="container">
-        <Contacts />
+        <Header branding="Contact Manager" />
+        <div className="container">
+          <Contacts />
+        </div>
       </div>
-    </div>
+    </Provider>
   );
 }
 
